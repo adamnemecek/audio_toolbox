b1781b9693c70ad28cd8fdb946c6cfa1fc6e6d3d
adamnemecek <adamnemecek@gmail.com>
2021-01-28 13:47:39 -0800

    copied headers over

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,101 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// pub mod prelude;
+
+// mod au_audio_unit;
+// pub use au_audio_unit::*;
+
+// mod au_audio_unit_implementation;
+// pub use au_audio_unit_implementation::*;
+
+// mod au_cocoa_ui_view;
+// pub use au_cocoa_ui_view::*;
+
+// mod au_component;
+// pub use au_component::*;
+
+// mod audio_codec;
+// pub use audio_codec::*;
+
+// mod audio_component;
+// pub use audio_component::*;
+
+// mod audio_converter;
+// pub use audio_converter::*;
+
+// mod audio_file;
+// pub use audio_file::*;
+
+// mod audio_file_component;
+// pub use audio_file_component::*;
+
+// mod audio_file_stream;
+// pub use audio_file_stream::*;
+
+// mod audio_format;
+// pub use audio_format::*;
+
+// mod audio_hardware_service;
+// pub use audio_hardware_service::*;
+
+// mod audio_output_unit;
+// pub use audio_output_unit::*;
+
+// mod audio_queue;
+// pub use audio_queue::*;
+
+// mod audio_services;
+// pub use audio_services::*;
+
+// mod audio_session;
+// pub use audio_session::*;
+
+// mod audio_toolbox;
+// pub use audio_toolbox::*;
+
+// mod audio_unit;
+// pub use audio_unit::*;
+
+// mod audio_unit_carbon_view;
+// pub use audio_unit_carbon_view::*;
+
+// mod audio_unit_parameters;
+// pub use audio_unit_parameters::*;
+
+// mod audio_unit_properties;
+// pub use audio_unit_properties::*;
+
+// mod audio_unit_utilities;
+// pub use audio_unit_utilities::*;
+
+// mod audio_work_interval;
+// pub use audio_work_interval::*;
+
+// mod au_graph;
+// pub use au_graph::*;
+
+// mod aumidi_controller;
+// pub use aumidi_controller::*;
+
+// mod au_parameters;
+// pub use au_parameters::*;
+
+// mod caf_file;
+// pub use caf_file::*;
+
+// mod ca_show;
+// pub use ca_show::*;
+
+// mod core_audio_clock;
+// pub use core_audio_clock::*;
+
+// mod default_audio_output;
+// pub use default_audio_output::*;
+
+// mod extended_audio_file;
+// pub use extended_audio_file::*;
+
+// mod music_device;
+// pub use music_device::*;
+
+// mod music_player;
+// pub use music_player::*;
+
